03c832393a403b8f47ae7fbae956c10206287002
lukas.knapp <lukas.knapp@DE-DUS-MOB-123>
2017-03-08 12:09:17 +0100

    Add project files.

---
+++ RSSReader/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RSSReader.Startup))]
+namespace RSSReader
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
